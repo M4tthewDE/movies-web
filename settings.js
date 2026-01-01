95b832b8ad4632ddbca6d3e86844943074927b0b
M4tthewDE <matthias.kronberg@gmail.com>
2026-01-01 17:04:47 +0100

    feat: display censored tmdbApiKey in settings if set

--- settings.js
+++ settings.js
@@ -4,10 +4,23 @@ function onSaveSettings() {
 	d.setTime(d.getTime() + (31 * 24 * 60 * 60 * 1000));
 	let expires = "expires=" + d.toUTCString();
 	document.cookie = "tmdbApiKey=" + tmdbApiKey + ";" + expires + ";path=/";
+
+	const template = document.querySelector("#save-settings-result-template");
+	const clone = document.importNode(template.content, true);
+	let label = clone.querySelector("label");
+	label.innerText = "Success!";
+
+	let settings = document.querySelector("#settings");
+	settings.appendChild(clone);
 }
 
 function init() {
 	document.getElementById("save-settings-button").onclick = onSaveSettings;
+
+	let tmdbApiKey = getCookie("tmdbApiKey");
+	if (tmdbApiKey != "" && tmdbApiKey != null) {
+		document.getElementById("tmdb-api-key-input").value = "*".repeat(tmdbApiKey.length);
+	}
 }
 
 
